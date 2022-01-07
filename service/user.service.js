ea0a810877c1c58f5367fd2661c678c136411422
China-quanda <877880098@qq.com>
2022-01-07 16:04:09 +0800

    完成了登录功能

--- service/user.service.js
+++ service/user.service.js
@@ -13,27 +13,28 @@ class UserService {
     return res.dataValues
   }
 
-  // async getUserInfo({...args}){
-  //   const whereOpt = {...args}
-
-  // }
-
-  async getUserInfo({ user_name, email, mobile, is_admin, stutas, }) {
-    const whereOpt = {}
-    user_name && Object.assign(whereOpt, { user_name })
-    email && Object.assign(whereOpt, { email })
-    mobile && Object.assign(whereOpt, { mobile })
-    is_admin && Object.assign(whereOpt, { is_admin })
-    stutas && Object.assign(whereOpt, { stutas })
-
+  async getUserInfo({...args}){
+    const whereOpt = {...args}
     const res = await User.findOne({ 
-      attributes:['user_name','email','mobile','is_admin','stutas','password','paypassword'],//
-      where: whereOpt
-     });
-    return res ? res.dataValues : null
-
+          attributes:['user_name','email','mobile','is_admin','stutas','password','paypassword'],//
+          where: whereOpt
+         });
+        return res ? res.dataValues : null
   }
+  // async getUserInfo({ user_name, email, mobile, is_admin, stutas, }) {
+  //   const whereOpt = {}
+  //   user_name && Object.assign(whereOpt, { user_name })
+  //   email && Object.assign(whereOpt, { email })
+  //   mobile && Object.assign(whereOpt, { mobile })
+  //   is_admin && Object.assign(whereOpt, { is_admin })
+  //   stutas && Object.assign(whereOpt, { stutas })
 
+  //   const res = await User.findOne({ 
+  //     attributes:['user_name','email','mobile','is_admin','stutas','password','paypassword'],//
+  //     where: whereOpt
+  //    });
+  //   return res ? res.dataValues : null
+  // }
 }
 
 module.exports = new UserService()
